fc6ff721262516fe2dd434e7ea695ce039fabdb6
MartynasJanu <martynas.janu@gmail.com>
2015-08-18 23:38:25 +0300

    main.cpp initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Created on August 19, 2015, 12:13 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
